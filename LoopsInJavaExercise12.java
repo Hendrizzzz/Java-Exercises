8571f34968965f2353d38935e97cd899ced2d37a
Hendrizzzz <jimhendrixbageo@gmail.com>
2023-09-01 13:27:05 +0800

    improved code

--- LoopsInJavaExercise12.java
+++ LoopsInJavaExercise12.java
@@ -2,27 +2,30 @@ package NesoAcademy;
 
 import java.util.Scanner;
 
-public class LoopsInJavaExercise12 {
+public class LoopsInJavaExercise12{
     public static void main(String[] args) {
-
-        int userNumber;
         int i;
         int j;
+        int k;
+        int userInput;
         String x = "";
-        String asterisk = "*";
-        String row = "";
+        String y = "";
 
         Scanner scanner = new Scanner (System.in);
-        System.out.println("Give a number: ");
-        userNumber = scanner.nextInt();
+        System.out.println("Enter a number: ");
+        userInput = scanner.nextInt();
 
-        for (i = 1; i <= userNumber; i++){
-            for (j = i; j < userNumber; j++){
-                x += (" ");
+        for (i = 1; i <= userInput; i++){            //for the amount of lines
+
+            for (j = 1; j <= userInput - i; j++){    //for the amount of spaces in each line
+                x += " ";
             }
-            row = asterisk.repeat(i);
-            System.out.println(x + row);
-            x = "";
+            for (k = 1; k <= i; k++){
+                y += "*";
+            }
+            System.out.print(x + y);
+            System.out.println();
+            x = ""; y = "";
         }
     }
 }
