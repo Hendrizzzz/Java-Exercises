4cc68e6195f1267c2143328e70e993834801c603
Hendrizzzz <jimhendrixbageo@gmail.com>
2023-09-02 20:00:06 +0800

    second commit (more efficient code)

---
+++ CreatingClassesAndMethod.java
@@ -0,0 +1,17 @@
+package NesoAcademy;
+
+public class CreatingClassesAndMethod {
+    public static void main(String[] args) {
+        float age = 18;
+        me.sayName();
+        sayAge();
+        System.out.println(age);
+    }
+    private static void sayName() {
+        System.out.println("Jim Hendrix");
+        return;
+    }
+    public static void sayAge() {
+        System.out.println("18");
+    }
+}
---
+++ me.java
@@ -0,0 +1,10 @@
+package NesoAcademy;
+
+public class me {
+    public static void sayName() {
+        System.out.println("Jim Hendrix");
+    }
+    public static void sayAge(){
+        System.out.print(18);
+    }
+}
