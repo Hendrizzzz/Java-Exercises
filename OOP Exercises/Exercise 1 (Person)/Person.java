61df5307b3a186f55643a8877f7223f1e4ba1919
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2024-01-09 02:45:45 +0800

    Create Person.java

---
+++ OOP Exercises/Exercise 1 (Person)/Person.java
@@ -0,0 +1,19 @@
+package OOP.Exercises.Person;
+
+public class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
